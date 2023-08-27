498d1a658869be9addfb24302d012f81f72adf25
akshay22bv <akshay22bv@gmail.com>
2023-08-27 14:44:20 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -8,36 +8,12 @@ import ContactUs from "@/components/ContactUs";
 import Clients from "@/components/Clients";
 import Home from "@/components/Home";
 import Tools from "@/components/Tools";
-import { BsFillArrowUpCircleFill } from "react-icons/bs";
-import { Link } from "react-scroll";
+import Floter from "./common/Floter";
 
 const page = () => {
-  const [scrolling, setScrolling] = useState(window.scrollY > 30);
-
-  const onScroll = () => {
-    setScrolling(window.scrollY > 30);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, []);
-
   return (
     <div>
-      {scrolling && (
-        <Link
-          to="hero"
-          className="fixed bottom-10 right-5 animate-bounce z-50 cursor-pointer  text-green-400"
-          smooth={true}
-          duration={500}
-        >
-          <BsFillArrowUpCircleFill size={30} />
-        </Link>
-      )}
+      <Floter />
       <div className="bg-gradient-to-tr from-white via-blue-100  to-white">
         <Navbar />
         <Home />
